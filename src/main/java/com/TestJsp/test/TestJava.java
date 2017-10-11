904c755dea9a59863bd6922e6cfebad8d9e36054
weiLaoBiao <503910256@qq.com>
2017-10-11 13:15:16 +0800

    TestJava 修改了

---
+++ src/main/java/com/TestJsp/test/TestJava.java
@@ -0,0 +1,12 @@
+package com.TestJsp.test;
+
+/**
+ * Created by RXLiuli on 2017/9/29.
+ * 测试类
+ */
+public class TestJava {
+    public static void main(String[] args) {
+//        System.out.println(DaoExtension.getConn());
+        System.out.println("hello world");
+    }
+}
